9bd9c34cd62aebe2a7ba7c3b5e9c174477712fef
Eugenio Carvalho <carvalhoalberto03@gmail.com>
2021-12-05 16:13:15 -0300

    feat: todo list application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as C from './Appstyles';
+import { AddArea } from './components/AddArea';
+
+import { ListItem } from './components/ListItem';
+import { Item } from './types/Item';
+
 
 const  App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Comprar o pão na padaria', done: true},
+    {id: 1, name: 'Comprar um bolo na padaria', done: false}
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+
+    setList(newList);
+  } 
+  
   return (
-    <div className="App">
-      ola mundo!
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        <AddArea onEnter={handleAddTask}/>
+        {list.map((item, index) =>(
+          <ListItem key={index} item={item} />
+        ))}
+
+      </C.Area>
+    </C.Container>
   );
 }
 
