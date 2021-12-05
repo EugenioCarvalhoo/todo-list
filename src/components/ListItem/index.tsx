6d0a1f2fde610b1c77cad673503aee0d85798ffc
Eugenio Carvalho <carvalhoalberto03@gmail.com>
2021-12-05 16:54:17 -0300

    refctor: ajustesno done do obj item

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -9,14 +9,14 @@ type Props = {
 export const ListItem = ({item}: Props) => {
 
     const [isChecked ,setChecked] = useState(item.done);
-    
+    item.done = isChecked;
     return (
        <C.Container done={isChecked}>
            <input type="checkbox" 
                 checked={isChecked} 
                 onChange={e => setChecked(e.target.checked)}
            />
-          < label>{item.name}</label>
+          < label>{item.name} - {item.id}</label>
        </C.Container>
     );
 }
